c0cfbe52a1ecaf329749a14e8fb54bcfc6a7253c
Zekus63 <gregoire.marion@epitech.eu>
2026-05-19 15:44:58 +0200

    feat: add planning component in planning tab

--- apps/employer-app/src/app/(tabs)/Planning.tsx
+++ apps/employer-app/src/app/(tabs)/Planning.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from 'react-native';
+import CalendarComponent from '@/components/planning/calendar';
+import { colors } from '@/constants/theme';
+import { Text, View, ScrollView, useColorScheme } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
 
 export default function Planning() {
+  const colorScheme = useColorScheme();
+
+  const isDark = colorScheme === 'dark';
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Planning!</Text>
-    </View>
+    <ScrollView className="bg-white dark:bg-primary pt-10 bg-surface flex-1">
+      <Text className="text-4xl font-bold pt-6 px-6 dark:text-white">Mon Planning</Text>
+
+      <View className="flex-1 m-6 rounded-lg">
+        <Shadow
+          startColor={isDark ? '#FFFFFF10' : '#00000010'}
+          style={{
+            borderRadius: 24,
+            width: '100%',
+            marginBottom: 16,
+            backgroundColor: isDark ? colors.background.dark : colors.background.light,
+          }}
+        >
+          <CalendarComponent />
+        </Shadow>
+      </View>
+    </ScrollView>
   );
 }
