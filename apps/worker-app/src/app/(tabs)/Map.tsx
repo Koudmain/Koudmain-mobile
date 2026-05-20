4fe14686531efaf144f3e2ae966e6762a4f4e466
yanismgt <yanis.mignot@epitech.eu>
2026-05-20 16:40:58 +0200

    feat: new image pin and fix map with google map

--- apps/worker-app/src/app/(tabs)/Map.tsx
+++ apps/worker-app/src/app/(tabs)/Map.tsx
@@ -1,9 +1,14 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
-import MapView, { Marker, Region } from 'react-native-maps';
+import { View, StyleSheet, FlatList, useColorScheme, Image } from 'react-native';
+import MapView, { Marker, Region, PROVIDER_GOOGLE } from 'react-native-maps';
 import { DynamicMarker } from '@/svg/DynamicMarker';
 import { useSession } from '@/context/SessionContext';
 import { apiFetch } from '@/utils/api';
+import { darkMapStyle, lightMapStyle } from '@/constants/styleMap';
+
+import markerLight from '@/assets/images/map/pin_black.png';
+import markerDark from '@/assets/images/map/pin_white.png';
+import markerSelected from '@/assets/images/map/pin_green.png';
 
 interface Publication {
   id: number;
@@ -22,18 +27,34 @@ export default function MapScreen() {
   const mapRef = useRef<MapView>(null);
   const flatListRef = useRef<FlatList>(null);
   const { session } = useSession();
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === 'dark';
+  const currentMapStyle = isDark ? darkMapStyle : lightMapStyle;
 
   const [publications, setPublications] = useState<Publication[]>([]);
   const [selectedId, setSelectedId] = useState<number | null>(null);
-  const debounceTimerRef = useRef<NodeJS.Timeout | null>(null);
   const [tracksView, setTracksView] = useState(true);
+  const isClickingMarkerRef = useRef(false);
+  const debounceTimerRef = useRef<NodeJS.Timeout | null>(null);
 
   const fetchAddresses = useCallback(
-    async (currentRegion: Region) => {
-      const min_lat = currentRegion.latitude - currentRegion.latitudeDelta / 2;
-      const max_lat = currentRegion.latitude + currentRegion.latitudeDelta / 2;
-      const min_lng = currentRegion.longitude - currentRegion.longitudeDelta / 2;
-      const max_lng = currentRegion.longitude + currentRegion.longitudeDelta / 2;
+    async (options: {
+      region?: Region;
+      boundaries?: { min_lat: number; max_lat: number; min_lng: number; max_lng: number };
+    }) => {
+      let min_lat, max_lat, min_lng, max_lng;
+
+      if (options.boundaries) {
+        ({ min_lat, max_lat, min_lng, max_lng } = options.boundaries);
+      } else if (options.region) {
+        const { latitude, longitude, latitudeDelta, longitudeDelta } = options.region;
+        min_lat = latitude - latitudeDelta / 2;
+        max_lat = latitude + latitudeDelta / 2;
+        min_lng = longitude - longitudeDelta / 2;
+        max_lng = longitude + longitudeDelta / 2;
+      } else {
+        return;
+      }
 
       try {
         const data = await apiFetch<Publication[]>(
@@ -55,7 +76,7 @@ export default function MapScreen() {
   );
 
   useEffect(() => {
-    fetchAddresses(BELLECOUR_REGION);
+    fetchAddresses({ region: BELLECOUR_REGION });
 
     return () => {
       if (debounceTimerRef.current) clearTimeout(debounceTimerRef.current);
@@ -74,29 +95,77 @@ export default function MapScreen() {
     }
   }, [publications]);
 
-  const handleRegionChangeComplete = (region: Region) => {
+  const handleRegionChangeComplete = async (region: Region) => {
     if (debounceTimerRef.current) {
       clearTimeout(debounceTimerRef.current);
     }
 
-    debounceTimerRef.current = setTimeout(() => {
-      fetchAddresses(region);
+    if (isClickingMarkerRef.current) {
+      isClickingMarkerRef.current = false;
+      return;
+    }
+
+    debounceTimerRef.current = setTimeout(async () => {
+      if (!mapRef.current) return;
+
+      try {
+        const boundaries = await mapRef.current.getMapBoundaries();
+
+        const min_lat = boundaries.southWest.latitude;
+        const max_lat = boundaries.northEast.latitude;
+        const min_lng = boundaries.southWest.longitude;
+        const max_lng = boundaries.northEast.longitude;
+
+        await fetchAddresses({ boundaries: { min_lat, max_lat, min_lng, max_lng } });
+      } catch (error) {
+        console.error('Impossible de récupérer les limites de la carte :', error);
+      }
     }, 800);
   };
 
-  const centerMapOnMarker = (lat: number, lng: number) => {
-    mapRef.current?.animateToRegion(
-      {
-        latitude: lat,
-        longitude: lng,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-      },
-      600,
-    );
+  const centerMapOnMarker = async (lat: number, lng: number) => {
+    if (!mapRef.current) return;
+
+    try {
+      const boundaries = await mapRef.current.getMapBoundaries();
+
+      const currentLatDelta = Math.abs(
+        boundaries.northEast.latitude - boundaries.southWest.latitude,
+      );
+      const currentLngDelta = Math.abs(
+        boundaries.northEast.longitude - boundaries.southWest.longitude,
+      );
+
+      const IDEAL_DELTA = 0.005;
+
+      const finalLatDelta = currentLatDelta < IDEAL_DELTA ? currentLatDelta : IDEAL_DELTA;
+      const finalLngDelta = currentLngDelta < IDEAL_DELTA ? currentLngDelta : IDEAL_DELTA;
+
+      mapRef.current.animateToRegion(
+        {
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: finalLatDelta,
+          longitudeDelta: finalLngDelta,
+        },
+        350,
+      );
+    } catch (error) {
+      mapRef.current.animateToRegion(
+        {
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        },
+        350,
+      );
+    }
   };
 
   const handleMarkerPress = (pub: Publication, index: number) => {
+    isClickingMarkerRef.current = true;
+
     setSelectedId(pub.id);
     centerMapOnMarker(pub.latitude, pub.longitude);
     flatListRef.current?.scrollToIndex({
@@ -110,26 +179,30 @@ export default function MapScreen() {
     <View style={styles.container}>
       <MapView
         ref={mapRef}
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={BELLECOUR_REGION}
         showsPointsOfInterest={false}
         onRegionChangeComplete={handleRegionChangeComplete}
+        customMapStyle={currentMapStyle}
       >
-        {publications.map((pub, index) => (
-          <Marker
-            key={pub.id}
-            coordinate={{ latitude: pub.latitude, longitude: pub.longitude }}
-            onPress={() => handleMarkerPress(pub, index)}
-            tracksViewChanges={tracksView}
-          >
-            <DynamicMarker
-              multiple={false}
-              isSelected={selectedId === pub.id}
-              width={40}
-              height={40}
-            />
-          </Marker>
-        ))}
+        {publications.map((pub, index) => {
+          const isSelected = selectedId === pub.id;
+
+          let markerImage = isDark ? markerDark : markerLight;
+          if (isSelected) {
+            markerImage = markerSelected;
+          }
+          return (
+            <Marker
+              key={pub.id}
+              coordinate={{ latitude: pub.latitude, longitude: pub.longitude }}
+              onPress={() => handleMarkerPress(pub, index)}
+            >
+              <Image source={markerImage} style={{ width: 40, height: 40 }} resizeMode="contain" />
+            </Marker>
+          );
+        })}
       </MapView>
     </View>
   );
@@ -144,3 +217,4 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
+600;
