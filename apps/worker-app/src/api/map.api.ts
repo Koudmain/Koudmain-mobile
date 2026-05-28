3f4d39faf86f3f7a4e37cf6df195372163b51aa9
yanismgt <yanis.mignot@epitech.eu>
2026-05-28 08:40:37 +0200

    feat: new route api

---
+++ apps/worker-app/src/api/map.api.ts
@@ -0,0 +1,20 @@
+import { PublicationMap } from '@/types/publication';
+import { apiFetch } from '@/utils/api';
+
+export const mapService = {
+  fetchPublicationsInBounds: async (
+    token: string | null,
+    min_lat: number,
+    max_lat: number,
+    min_lng: number,
+    max_lng: number,
+  ) => {
+    return apiFetch<PublicationMap[]>(
+      `/address/map?min_lat=${min_lat}&max_lat=${max_lat}&min_lng=${min_lng}&max_lng=${max_lng}`,
+      {
+        method: 'GET',
+        token: token ?? undefined,
+      },
+    );
+  },
+};
