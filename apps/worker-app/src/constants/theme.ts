6cce960131a358d43ac924bd9f580a93d5a3d8d3
yanismgt <yanis.mignot@epitech.eu>
2026-06-01 11:14:41 +0200

    fix: theme

--- apps/worker-app/src/constants/theme.ts
+++ apps/worker-app/src/constants/theme.ts
@@ -1,3 +1,3 @@
-import tailwindConfig from '../../../../packages/koudmain-ui/tailwind.config';
+import tailwindConfig from '@/tailwind.config';
 
 export const colors: any = tailwindConfig?.theme?.extend?.colors || {};
