8739f3cb870afdfee3a84a09a0b9fa96c07898d2
Maxime Vaultier <mxm.vaultier@gmail.com>
2026-09-07 18:15:33 +0100

    refactor(theme): re-export colors from @koudmain/ui for clean typing

--- apps/worker-app/src/constants/theme.ts
+++ apps/worker-app/src/constants/theme.ts
@@ -1,4 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const tailwindConfig = require('../../../../packages/koudmain-ui/tailwind.config');
-
-export const colors = tailwindConfig.theme.extend.colors;
+export { colors } from '@koudmain/ui';
